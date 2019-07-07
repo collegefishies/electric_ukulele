a466609373d68c231ab4948f3d49f6186fcf2646
Enrique Mendez <eqm@mit.edu>
2019-07-07 14:01:39 -0400

    Attempt at non blocking FFT

--- electric_Ukulele.ino
+++ electric_Ukulele.ino
@@ -1,12 +1,27 @@
 
 #include <Adafruit_NeoPixel.h>
+#include <arduinoFFT.h>
 
+//hardware interface
 const int micPin = A2;
 const int powerPin = 10;
 const int dacPin = A0;
 const int ledPin = 5;
 const int ledCount = 15;
 const int batteryPin = A7;
+
+//fft parameters
+const int samplerate = 5000;
+const int fftSize = 256;
+const int spectrumSize = fftSize / 2;
+const unsigned int sampling_period_us = round(1e6/samplerate);
+
+double vReal[fftSize];
+double vImag[fftSize];
+
+arduinoFFT FFT = arduinoFFT();
+
+
 
 void setup() {
   Serial.begin(20000000);
@@ -24,7 +39,13 @@ void loop() {
   double dblPass = 0;
   double oldBandpass = 0;
   int oldtime = micros();
+  unsigned long oldSampleTime = micros();
+  unsigned long sampleTime = micros();
+
+  int sampleI = 0;
   while(1){
+
+    /*** Filter the input ***/
     sensorVal = analogRead(micPin);
     oldLowpass = lowpass;
     oldBandpass = bandpass;
@@ -33,10 +54,41 @@ void loop() {
     bandpass = bandpass - (micros() - oldtime)*hiCutoff*(bandpass) + lowpass - oldLowpass; //first hi pass
     dblPass = dblPass - (micros() - oldtime)*hiCutoff*(dblPass) + bandpass - oldBandpass; //second hi pass
 
-    analogWrite(dacPin,(int) 2*round(dblPass) + 500);
-    Serial.println((int) round(dblPass));
+    analogWrite(dacPin,(int) round(2*dblPass) + 500);
+    Serial.println(dblPass);
 
     oldtime = micros(); 
+
+    /*** Non blocking sampling of input ***/
+    sampleTime = oldtime;
+
+    if(sampleTime - oldSampleTime > sampling_period_us){
+      vReal[sampleI] = dblPass;
+      vImag[sampleI] = 0;
+
+      sampleI = (sampleI + 1);
+      oldSampleTime = sampleTime;
+    }
+
+    /*** FFT ***/
+    if(sampleI != fftSize){
+      continue;
+    }
+
+    continue;
+
+    sampleI = 0;
+    for(int i = 0; i < fftSize/2; i++){
+      //Serial.println((vReal[i]));
+    }
+
+    FFT.Windowing(vReal, fftSize, FFT_WIN_TYP_BLACKMAN_HARRIS, FFT_FORWARD);
+    FFT.Compute(vReal,vImag, fftSize, FFT_FORWARD);
+    FFT.ComplexToMagnitude(vReal,vImag, fftSize);
+
+    //print results
+    
+    
   }
 }
 
